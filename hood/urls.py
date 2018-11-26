5195ba80fe227f3765956b2bacc8f2488ed106cc
kevahere <kevahere@gmail.com>
2018-11-26 14:04:06 +0300

    added url to search

--- hood/urls.py
+++ hood/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$',views.index,name = 'home'),
-    #url(r'^profile/',views.profile,name= 'profile')
+    url(r'^$',views.index,name ='home'),
+    url(r'^profile/',views.profile,name= 'profile')
+    url(r'^business/'views.create_business(),name='create_business'),
+    url(r'^new/post$', views.new_post, name='newpost'),
+    url(r'^search/$', views.search, name='search'),
+
 ]
