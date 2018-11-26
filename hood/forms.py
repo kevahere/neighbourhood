77ad2b8298429600241e17e9c42ddd57f3fa21f7
kevahere <kevahere@gmail.com>
2018-11-26 10:16:50 +0300

    added forms logic

---
+++ hood/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import *
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user']
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('name','user_name','bio')
+
+class CreateHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        fields = ['name','location','occupants']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name','bio']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model  = Business
+        fields = ['name','hood','email']
